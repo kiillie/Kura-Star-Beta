57dd545959467511e7d6ba2ea2971437e260ce58
kiillie <drnneri27@gmail.com>
2015-02-02 18:06:13 -0800

    Change the upload again

--- app/views/articles/file_upload.blade.php
+++ app/views/articles/file_upload.blade.php
@@ -2,11 +2,11 @@
 if(isset($image['image'])){
 	$upload = $image['image'];
 	$name = str_random(40);
-	$folder = public_path()."\assets\images\attachments\\";
+	$folder = public_path()."/assets/images/attachments/";
 	$move = $name.".".$upload->getClientOriginalExtension();
 
 	//echo $upload->getRealPath();
 	$upload->move($folder, $move);
-	echo "\assets\images\attachments\\".$move;
+	echo "/assets/images/attachments/".$move;
 }
 ?>
