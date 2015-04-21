8e7ab3d8e0633f53c6dd1cd34c873ae3dd491a8c
kiillie <drnneri27@gmail.com>
2015-04-21 08:56:17 +0800

    DB

--- app/views/public/login.blade.php
+++ app/views/public/login.blade.php
@@ -1,37 +1,36 @@
 @extends('layouts.main')
 @section('content')
-<div class="container">
-	<div class="reglog-wrapper row">
-		<div class="col-md-12">
-			<div class="log-form">
-				<h2>Login</h2>
-					@if(Session::has('message_login'))
-					<div class="form-group">
-						<span class="label label-danger">{{Session::get('message_login')}}</span>
-					</div>
-					@endif
-				{{Form::open(['name' => 'login', 'route' => 'user.login'])}}
-					<div class="form-group">
-							{{Form::text('log_email', '', ['class' => 'form-control', 'placeholder' => 'Email Address'])}}
-					</div>
-					<div class="form-group">
-						{{Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password'])}}
-					</div>
-					<div class="form-group">
-						{{Form::checkbox('remember')}}
-						{{Form::label('remember', 'Remember Me', ['style' => 'vertical-align:top; font-weight: normal;'])}}
-					</div>
-					<div>{{Form::submit('Login', ['class'	=> 'form-control btn btn-primary'])}}</div>
-				{{Form::close()}}
-				<div class="social-login row">
-					<br/>
-					<h6>Or Login with:</h6>
-					<div class="facebook col-md-6">
-						<a href="fbauth"><img src="/assets/images/facebook.png"></a>
-					</div>
-				</div>
+<div class="defaultWidth center clear-auto bodycontent registration">
+	<div class="log-form">
+		<h2 class="title">Already have an account?</h2>
+			@if(Session::has('message_login'))
+			<div class="form-group">
+				<span class="label label-danger">{{Session::get('message_login')}}</span>
+			</div>
+			@endif
+		{{Form::open(['name' => 'login', 'route' => 'user.login'])}}
+			<div class="form-group">
+					{{Form::text('log_email', '', ['class' => 'form-control', 'placeholder' => 'Email Address'])}}
+			</div>
+			<div class="form-group">
+				{{Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password'])}}
+			</div>
+			<div class="form-group">
+				{{Form::checkbox('remember')}}
+				&nbsp;{{Form::label('remember', ' Remember Me')}}
+			</div>
+			<div>{{Form::submit('Login', ['class'	=> 'form-control btn btn-primary'])}}</div>
+		{{Form::close()}}
+		<div class="social-login">
+			<br/>
+			<h6>Or Login with:</h6>
+			<div class="facebook">
+			   <a href="fbauth"><img src="/assets/images/facebook.png"></a>
+			</div>
+			<div class="twitter">
+				{{HTML::image('assets/images/twitter.png')}}
 			</div>
 		</div>
-	</div> 
+	</div>
 </div>
 @stop
