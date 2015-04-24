4fc963f3caf987d5638832797632dce79f73dc45
kiillie <drnneri27@gmail.com>
2015-04-24 16:40:45 +0800

    image

--- app/views/articles/create.blade.php
+++ app/views/articles/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('content')
 <script type="text/javascript" src="/assets/js/plugins/jquery.fancybox.js?v=2.1.5"></script>
-<link rel="stylesheet" type="text/css" href="/assets/css/plugins/jquery.fancybox.css?v=2.1.5" media="screen" />
+<link rel="stylesheet" type="text/css" href="/assets/css/plugins/jquery.fancybox.css?v=2.1.5" media="screen" /> 
 <script>
 $(document).ready(function(){
 	$(".art-added-img").fancybox({
