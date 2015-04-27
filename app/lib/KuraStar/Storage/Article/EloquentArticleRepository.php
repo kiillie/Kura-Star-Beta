10b13eaa64f0231d7fa7a86a049e8672554d37ea
kiillie <drnneri27@gmail.com>
2015-04-27 15:14:36 +0800

    This is it

--- app/lib/KuraStar/Storage/Article/EloquentArticleRepository.php
+++ app/lib/KuraStar/Storage/Article/EloquentArticleRepository.php
@@ -41,12 +41,12 @@ class EloquentArticleRepository implements ArticleRepository{
 				$parts = pathinfo($url['path']);
 				$filename = $parts['filename'].".".$parts['extension'];
 				$path = public_path()."/assets/images/attachments/".$filename;
-				$file = "/assets/images/attachmentsssss/".$filename;
+				$file = "/assets/images/attachments/".$filename;
 				if(file_exists($path)){
 					$rand = str_random(7);
 					$filename = $parts['filename']."_".$rand.".".$parts['extension'];
 					$path = public_path()."/assets/images/attachments/".$filename;
-					$file = "/assets/images/attachmentsssss/".$filename;
+					$file = "/assets/images/attachments/".$filename;
 				}
 				if(fopen($path, "w")){
 					if(\File::copy($input['imageUrl'], $path)){
