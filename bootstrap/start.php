6232dc3d28621adf9bccb2ebf9eed273ec238b96
kiillie <drnneri27@gmail.com>
2015-05-29 15:38:57 +0800

    db

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,9 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('PC01-PC'),
-	'production' => array('m_suzuki'),
-	'staging' => array('kurastar.com'),
+	'local' => array('PC01-PC', 'm_suzuki'),
+	'production' => array('kurastar.com'),
 
 ));
 
